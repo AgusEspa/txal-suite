dd9821af9003c5517f96ada116e059e2abdb33c4
Agustin Espana <agustinespania@gmail.com>
2021-12-28 09:50:36 -0300

    added test cases

--- src/main/java/me/projects/texal/TextParser.java
+++ src/main/java/me/projects/texal/TextParser.java
@@ -6,12 +6,17 @@ import java.util.List;
 public class TextParser {
     
     public static List<Paragraph> parse(final String text) {
+
         final List<Paragraph> paragraphs = new ArrayList<Paragraph>();
+
         final String[] split;
+
         final String[] rawParagraphs = split = text.split("\\r\\n|\\n|\\r");
+
         for (final String p : split) {
             paragraphs.add(new Paragraph(p));
         }
+
         return paragraphs;
     }
 }
