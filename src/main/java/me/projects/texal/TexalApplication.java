ac4124da6f429e2fffe5014c043092c9572faa50
Agustin Espana <agustinespania@gmail.com>
2021-12-27 23:26:02 -0300

    project directory

---
+++ src/main/java/me/projects/texal/TexalApplication.java
@@ -0,0 +1,13 @@
+package me.projects.texal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexalApplication.class, args);
+	}
+
+}
