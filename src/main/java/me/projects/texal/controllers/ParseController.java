29064ea6063834c483defb4f590637b510d7a2ca
Agustin Espana <agustinespania@gmail.com>
2021-12-31 12:05:43 -0300

    added controllers for parsing

--- src/main/java/me/projects/texal/controllers/ParseController.java
+++ src/main/java/me/projects/texal/controllers/ParseController.java
@@ -1,13 +1,20 @@
 package me.projects.texal.controllers;
 
-import org.springframework.stereotype.Controller;
+import me.projects.texal.Paragraph;
+import me.projects.texal.TextParser;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/parse")
 public class ParseController {
 
-//    @PostMapping
-//    public
+    @PostMapping
+    public List<Paragraph> analyze(@RequestBody String text) {
+        return TextParser.parse(text);
+    }
 }
