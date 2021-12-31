99a182d717f4f0fabe39b7917fe3701cce1a2dce
Agustin Espana <agustinespania@gmail.com>
2021-12-31 12:39:27 -0300

    Update TextAnalyzerService.java

--- src/main/java/me/projects/texal/services/TextAnalyzerService.java
+++ src/main/java/me/projects/texal/services/TextAnalyzerService.java
@@ -1,11 +1,16 @@
 package me.projects.texal.services;
 
+import me.projects.texal.components.Text;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TextAnalyzerService {
 
-    public static void analyse() {
+    public static void analyse(Text text) {
+
+        // for each paragraph
+        // 1. tokenizer
+        // 2. relevance
 
     }
 }
