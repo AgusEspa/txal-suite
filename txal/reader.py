997c138ada2f02d42e7bf13f7dc70a3c35b31fd4
Agustin Espana <agustinespania@gmail.com>
2022-05-26 19:21:35 -0300

    better text reader

--- txal/reader.py
+++ txal/reader.py
@@ -1,13 +1,14 @@
+import os
+
 
 def read_article(file_name):
-    file = open(file_name, "r")
+    file = open('./text.txt')
     filedata = file.readlines()
     article = filedata[0].split(". ")
     sentences = []
 
     for sentence in article:
-        print(sentence)
         sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
-    sentences.pop()
+        sentences.pop()
 
     return sentences
