19c604907ed8a1f5ded14fa3c3532928ffca274a
Agustin Espana <agustinespania@gmail.com>
2022-05-31 17:09:00 -0300

    Create Application.java

---
+++ src/main/java/me/projects/txal/Application.java
@@ -0,0 +1,13 @@
+package me.projects.txal;
+
+public class Application {
+
+    public static void main(String[] args) {
+        
+        // ask for a file
+
+        // parse the file
+
+        // output new file
+    }
+}
